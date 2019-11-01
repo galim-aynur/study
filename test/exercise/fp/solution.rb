ea8116c66150082c15947b94751778cca0d2cdfc
Aynur Galimov <galim-aynur@ya.ru>
2019-11-01 16:40:25 +0500

    Add solutions for fp

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,12 +5,27 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+        array = films_select(_array)
+        ratings = array.reduce(0) { |acc, f| f['rating_kinopoisk'].to_f + acc }
+        ratings / array.count
       end
 
       def chars_count(_films, _threshold)
-        0
+        array = _films.select do |f|
+          f['rating_kinopoisk'].to_f >= _threshold if f['rating_kinopoisk']
+        end
+        array.reduce(0) { |acc, f| f['name'].count('и') + acc }
       end
+
+      private
+      def films_select(films)
+        films.select do |f|
+          if f['country'] && f['rating_kinopoisk']
+            f['country'].split(',').size >= 2 && f['rating_kinopoisk'].to_f > 0
+          end
+        end
+      end
+
     end
   end
 end
